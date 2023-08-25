c8888424233b71e4ad6b7a6b6444787d39b45fc3
AnshD8619 <ansh.desai86@gmail.com>
2023-08-25 00:02:47 -0600

    Completed Sign in and out functionality

--- ecommerce-application/src/routes/navigation/navigation.component.jsx
+++ ecommerce-application/src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import "./navigation.styles.scss";
-import {ReactComponent as Logo} from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import {signOutUser} from "../../utils/firebase/firebase.utils";
 const Navigation = () => {
+  const { currentUser} = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <Logo className="logo"/>
+          <Logo className="logo" />
         </Link>
         <div className="links-container">
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
-          <Link className="nav-link" to="/Authentication">
-            Login
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>Sign Out</span>
+          ) : (
+            <Link className="nav-link" to="/Authentication">
+              Login
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
--- ecommerce-application/src/utils/firebase/firebase.utils.js
+++ ecommerce-application/src/utils/firebase/firebase.utils.js
@@ -4,7 +4,9 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
@@ -58,3 +60,7 @@ export const signInAuthWithEmailAndPassword = async(email, password) => {
   if(!email || !password) return;
   return await signInWithEmailAndPassword(auth, email, password);
 }
+
+export const signOutUser = () => signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
